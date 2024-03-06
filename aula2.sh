00146358673601f6a93454688baf706f5912d7e3
rodpmaia <rod.pmaia@gmail.com>
2024-03-06 00:16:28 -0300

    modified:   aula2.sh

--- aula2.sh
+++ aula2.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Aula 2 Algoritmos com ShellScript
 # var
-
+   #export nome
 # begin
-
+   echo "Digite seu nome: "
+   read nome 
+   echo "Olah! Seja bem vindo(a) ", $nome
 # end
