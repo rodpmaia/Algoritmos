ac292d2e54f4291ccb12a4a1279a6377f0a0b23d
rodpmaia <rod.pmaia@gmail.com>
2024-03-06 00:46:57 -0300

    new file:   aula3.sh

---
+++ aula3.sh
@@ -0,0 +1,12 @@
+#/usr/bin/env bash
+# Aula3 Soma 2 numeros
+
+# begin
+  #read -p  "Digite dois n√meros para efetuar a soma: " num1 num2
+  #echo "A soma entre:"  num1 "e" num2 "√©igual a: " expr ("$num1" + "$num2) 
+  read -p "Digite o primeiro numero: " numOne
+  read -p "Digite o segundo numero: " numTwo
+
+  echo "A soma entre $numOne mais $numTwo √ igual a:" $(expr "$numOne" + "$numTwo");
+# end
+
